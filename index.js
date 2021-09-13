60046671cf6b07bceccfe5c8aa95a544a4a383e1
damonelena <81850212+damonelena@users.noreply.github.com>
2021-09-13 13:05:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('MARAM CHANDU')
+  res.send('MARAM SOMbABU')
 })
 
 app.get('/status', (req, res) => {
